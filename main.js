b2242306c1ddc4e0320594c365b53be25fe40bf7
nmatos <nmatos@pursuit.org>
2024-03-27 23:25:29 -0400

    75% completed calculator

--- main.js
+++ main.js
@@ -1,26 +1,65 @@
 let currentDisplay = '0';
-let resultDisplay;
+let resultDisplay = false;
+let operator = 0;
+let operator1 = 0;
+let operator2 = 0;
+
 
 function calculatorDisplay(val) {
-    if(currentDisplay === '0' || resultDisplay == false) {
+    if (currentDisplay === '0' || resultDisplay || currentDisplay === '') {
         currentDisplay = val;
     } else {
-        currentDisplay += val
+        currentDisplay += val;
     }
     resultDisplay = false;
-
     updatedDisplay();
+    console.log(val);
 }
 
 function updatedDisplay() {
-    const display = document.querySelector('.calculator__display')
-
-    display.textContent = currentDisplay;
+    const display = document.querySelector('.calculator__display');
+    display.innerHTML = currentDisplay;
 }
 
-function calculateResult(operator) {
+function setOperator(val) {
+    if (currentDisplay !== '0' && !resultDisplay) {
+        operator1 = Number(currentDisplay);
+        operator = val;
+        currentDisplay = '';
+    }
+    console.log(val);
+}
 
-    
+function calculateResult() {
+    let result;
+    operator2 = Number(currentDisplay);
+
+    switch (operator) {
+        case '+':
+            result = operator1 + operator2;
+            break;
+        case '-':
+            result = operator1 - operator2;
+            break;
+        case '*':
+            result = operator1 * operator2;
+            break;
+        case '/': result = operator1 / operator2;
+            break;
+        default:
+            currentDisplay = 'Error: Invalid Operation';
+            updatedDisplay();
+            return;
+    }
+
+    currentDisplay = result.toString();
+    updatedDisplay();
+    resultDisplay = true;
+}
+
+function clearDisplay() {
+    currentDisplay = '0';
+    updatedDisplay();
 }
 
 
